241c3de32b53befe70b6d3756488e3ea9d3eb5c8
kayayakup <yakupyek13@mail.com>
2022-01-28 17:26:55 +0300

    C#101 practics

---
+++ Constructor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharpOgretici // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calisan c=new Calisan("büşra","kaya",3,"öğrenci");
+            c.Calisans();
+
+            Calisan c2=new Calisan("büşra","kaya");
+            c2.Calisans();
+        }
+    }
+
+    class Calisan{
+        public string ad;
+        public string soy;
+        public int yas;
+        public string departman;
+
+        public Calisan(string ad,string soy,int yas,string departman){
+            this.ad=ad;
+            this.soy=soy;
+            this.yas=yas;
+            this.departman=departman;
+        }
+
+        public Calisan(string ad,string soy){
+            this.ad=ad;
+            this.soy=soy;
+        }
+
+        public Calisan(){}
+
+        public void Calisans(){
+            Console.WriteLine("calisanın adı {0}",ad);
+            Console.WriteLine("calisanın soyadı {0}",soy);
+            Console.WriteLine("calisanın yası {0}",yas);
+            Console.WriteLine("calisanın depatman {0}",departman);
+        }
+    }
+}
